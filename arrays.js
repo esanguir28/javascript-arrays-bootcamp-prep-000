88d0aa16207d1a496bd38b03e1c1925066019d7f
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-09-17 03:41:03 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -3,13 +3,12 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
 var superheroines = ["catwoman", "she-hulk", "mystique"];
 superheroines.push("wonderwoman");
 
-var cities = ["New York", 'San Francisco']
-cities = ["Philadelphia",...cities] 
-
 const cats = ["Milo","Garfield"]
 const moreCats = ["Felix",...cats]
 
-function addElementToBeginningOfArray(cities,citystring){
+function addElementToBeginningOfArray(city,citystring){
+var cities = ["New York", 'San Francisco']
+cities = ["Philadelphia",...cities] 
   cities.unshift(citystring)
 }
   
