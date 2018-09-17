153ec146f814f891d4c0521931a0bc711d449831
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-09-17 03:30:11 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -2,3 +2,6 @@ var chocolateBars = ["snickers","hundred grand", "kitkat", "skittles"];
 
 var superheroines = ["catwoman", "she-hulk", "mystique"];
 superheroines.push("wonderwoman");
+
+var cities = ["New York", 'San Francisco']
+["Philadelphia",...cities] 
