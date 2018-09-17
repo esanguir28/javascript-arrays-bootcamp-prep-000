883dd96a4f9856e3beb7b3153b4d5b252a7f6e2f
Erica Janine Sanguir <ericajanisan@gmail.com>
2018-09-17 04:24:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -43,6 +43,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array){
 }
 
 function removeElementFromBeginningOfArray(array){
-array.slice(1)
-return array
+ array.slice(1)
+ return array
 }
